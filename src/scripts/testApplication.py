a6838d50c2c7b5d2e4c62dd0a21adc61999e6e11
fangc <saij84@gmail.com>
2018-11-26 19:25:27 +1100

    update

--- src/scripts/testApplication.py
+++ src/scripts/testApplication.py
@@ -1,1 +1,21 @@
-activ
+from PyQt5.QtWidgets import *
+
+
+app = QApplication([])
+
+window = QMainWindow()
+window.setWindowTitle("testAplication")
+window.resize(800, 650)
+
+label = QLabel("Style")
+
+qTopLayout = QHBoxLayout()
+qTopLayout.addWidget(label)
+
+window.addDockWidget(qTopLayout)
+window.show()
+
+
+
+
+app.exec_()
